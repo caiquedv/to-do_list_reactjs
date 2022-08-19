08aa1d8c3d83acb1b5561ec476c90c31614debd3
caique <caique.log@gmail.com>
2022-08-19 20:42:16 -0300

    Feat: Made Drag and Drop && deleted ListItem.

--- src/App.styles.ts
+++ src/App.styles.ts
@@ -20,3 +20,48 @@ export const Header = styled.h1`
     border-bottom: 1px solid #444;
     padding-bottom: 20px;
 `;
+
+interface ContainerProps {
+    done: boolean;
+}
+
+export const ContLabel = styled.label(({ done }: ContainerProps) => (`
+    display: flex;
+    align-items: center;
+    gap: 0.5rem;
+    background-color: #20212c;
+    padding: 5px;
+    border-radius: 10px;
+    margin-bottom: 10px;
+    color: #ccc;
+    position: relative;
+
+    span:not(.remove) {
+        text-decoration: ${done ? 'line-through' : 'initial'};
+    }
+
+    .task {
+        width: 70%
+    }
+    
+    .removeBox {
+        z-index: 1;
+        width: 70px;
+        height: 100%;
+        position: absolute;
+        right: 10px;
+        appearance: none;
+        cursor: pointer;
+        // background: red;
+
+        &:hover + .remove {
+            color: #666;            
+        }
+    }
+
+    .remove {
+        position: absolute;
+        right: 10px;
+        color: #444;        
+    }
+`))
--- src/App.tsx
+++ src/App.tsx
@@ -1,62 +1,93 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import * as C from './App.styles';
 import { AddArea } from "./components/AddArea/AddArea";
-import { ListItem } from "./components/ListItem/ListItem";
 import { Item } from "./types/Item";
 
 const App = () => {
-  const [list, setList] = useState<Item[]>([
-    { id: 1, name: 'Comprar o pão na padaria', done: false },
-    { id: 2, name: 'Comprar um bolo na padaria', done: true },
-  ]);
+	const [list, setList] = useState<Item[]>([
+		{ id: 1, name: 'Comprar o pão na padaria', done: false },
+		{ id: 2, name: 'Comprar um bolo na padaria', done: true },
+	]);
 
-  let newList: Item[];
+	let newList: Item[];
 
-  const handleAddTask = (taskName: string) => {
-    newList = [...list];
+	const handleAddTask = (taskName: string) => {
+		newList = [...list];
 
-    newList.push({
-      id: list.length + 1,
-      name: taskName,
-      done: false
-    });
+		newList.push({
+			id: list.length + 1,
+			name: taskName,
+			done: false
+		});
 
-    setList(newList);
-  };
+		setList(newList);
+	};
 
-  const handleDoneStatus = (id: number, checked: boolean) => {
-    newList = [...list];
+	const handleDoneStatus = (id: number, checked: boolean) => {
+		newList = [...list];
 
-    newList[id - 1].done = checked;
-    setList(newList);
-  };
+		newList[id - 1].done = checked;
+		setList(newList); //console.log(newList)
+	};
 
-  const removeTask = (id: number) => {
-    newList = [...list].filter(item => item.id !== id);
+	const removeTask = (id: number) => {
+		newList = [...list].filter(item => item.id !== id);
 
-    setList(newList);
-  };
+		setList(newList);
+	};
 
-  return (
-    <C.Container>
-      <C.Area>
-        <C.Header>Lista de Tarefas</C.Header>
+	// Drag and Drop Data <---///////////////////////////////////////////////////////////////////////////
 
-        <form onSubmit={e => e.preventDefault()}>
-          <AddArea onEnter={handleAddTask} />
+	const dragItem = useRef<any>(null);
+	const dragOverItem = useRef<any>(null);
 
-          {list.map((item, index) =>
-            <ListItem
-              key={index}
-              item={item}
-              onCheck={handleDoneStatus}
-              remove={removeTask}
-            />
-          )}
-        </form>
-      </C.Area>
-    </C.Container>
-  );
+	const handleSort = () => {
+		let orderList = [...list];
+
+		const draggedItemContent = orderList.splice(dragItem.current, 1)[0];
+
+		orderList.splice(dragOverItem.current, 0, draggedItemContent);
+
+		dragItem.current = null;
+		dragOverItem.current = null;
+
+		setList(orderList);
+	};
+
+
+	return (
+		<C.Container>
+			<C.Area>
+				<C.Header>Lista de Tarefas</C.Header>
+
+				<form onSubmit={e => e.preventDefault()}>
+					<AddArea onEnter={handleAddTask} />
+
+					{list.map((item, index) =>
+						<C.ContLabel key={index}
+							done={item.done}
+							draggable
+							onDragStart={(e) => (dragItem.current = index)}
+							onDragEnter={(e) => (dragOverItem.current = index)}
+							onDragEnd={handleSort}
+						>
+							<input
+								type="checkbox"
+								checked={item.done}
+								onChange={(e) => handleDoneStatus(item.id, e.target.checked)}
+							/>
+							<span className='task'>{item.name}</span>
+							<input className='removeBox'
+								type="checkbox"
+								onChange={() => removeTask(item.id)}
+							/>
+							<span className='remove'>Remover</span>
+						</C.ContLabel>
+					)}
+				</form>
+			</C.Area>
+		</C.Container>
+	);
 };
 
 export default App;
